4e48f3978c3ea53c24fcfebc98e4616e64bbcc1b
CostelusE <62852858+CostelusE@users.noreply.github.com>
2020-05-19 14:02:34 +0300

    Add files via upload

---
+++ Proiect/Proiect/Forma_Destinatie.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Proiect
+{
+    partial class Forma_Destinatie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Forma_Destinatie));
+            this.gbProvenienta = new System.Windows.Forms.GroupBox();
+            this.rbOlanda = new System.Windows.Forms.RadioButton();
+            this.rbItalia = new System.Windows.Forms.RadioButton();
+            this.rbGermania = new System.Windows.Forms.RadioButton();
+            this.rbSpania = new System.Windows.Forms.RadioButton();
+            this.rbAnglia = new System.Windows.Forms.RadioButton();
+            this.rbRomania = new System.Windows.Forms.RadioButton();
+            this.gbGrup = new System.Windows.Forms.GroupBox();
+            this.rbServici = new System.Windows.Forms.RadioButton();
+            this.rbFamilie = new System.Windows.Forms.RadioButton();
+            this.rbPrieteni = new System.Windows.Forms.RadioButton();
+            this.rbScoala = new System.Windows.Forms.RadioButton();
+            this.labelProvenienta = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.labelGrup = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonFinalizare = new System.Windows.Forms.Button();
+            this.gbProvenienta.SuspendLayout();
+            this.gbGrup.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbProvenienta
+            // 
+            this.gbProvenienta.BackColor = System.Drawing.Color.Gray;
+            this.gbProvenienta.Controls.Add(this.rbOlanda);
+            this.gbProvenienta.Controls.Add(this.rbItalia);
+            this.gbProvenienta.Controls.Add(this.rbGermania);
+            this.gbProvenienta.Controls.Add(this.rbSpania);
+            this.gbProvenienta.Controls.Add(this.rbAnglia);
+            this.gbProvenienta.Controls.Add(this.rbRomania);
+            this.gbProvenienta.Location = new System.Drawing.Point(60, 70);
+            this.gbProvenienta.Name = "gbProvenienta";
+            this.gbProvenienta.Size = new System.Drawing.Size(505, 183);
+            this.gbProvenienta.TabIndex = 4;
+            this.gbProvenienta.TabStop = false;
+            // 
+            // rbOlanda
+            // 
+            this.rbOlanda.AutoSize = true;
+            this.rbOlanda.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbOlanda.ForeColor = System.Drawing.Color.Black;
+            this.rbOlanda.Location = new System.Drawing.Point(303, 142);
+            this.rbOlanda.Name = "rbOlanda";
+            this.rbOlanda.Size = new System.Drawing.Size(96, 26);
+            this.rbOlanda.TabIndex = 6;
+            this.rbOlanda.TabStop = true;
+            this.rbOlanda.Text = "Olanda";
+            this.rbOlanda.UseVisualStyleBackColor = true;
+            // 
+            // rbItalia
+            // 
+            this.rbItalia.AutoSize = true;
+            this.rbItalia.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbItalia.ForeColor = System.Drawing.Color.Black;
+            this.rbItalia.Location = new System.Drawing.Point(303, 81);
+            this.rbItalia.Name = "rbItalia";
+            this.rbItalia.Size = new System.Drawing.Size(76, 26);
+            this.rbItalia.TabIndex = 4;
+            this.rbItalia.TabStop = true;
+            this.rbItalia.Text = "Italia";
+            this.rbItalia.UseVisualStyleBackColor = true;
+            // 
+            // rbGermania
+            // 
+            this.rbGermania.AutoSize = true;
+            this.rbGermania.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbGermania.ForeColor = System.Drawing.Color.Black;
+            this.rbGermania.Location = new System.Drawing.Point(303, 21);
+            this.rbGermania.Name = "rbGermania";
+            this.rbGermania.Size = new System.Drawing.Size(120, 26);
+            this.rbGermania.TabIndex = 5;
+            this.rbGermania.TabStop = true;
+            this.rbGermania.Text = "Germania";
+            this.rbGermania.UseVisualStyleBackColor = true;
+            // 
+            // rbSpania
+            // 
+            this.rbSpania.AutoSize = true;
+            this.rbSpania.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbSpania.ForeColor = System.Drawing.Color.Black;
+            this.rbSpania.Location = new System.Drawing.Point(25, 142);
+            this.rbSpania.Name = "rbSpania";
+            this.rbSpania.Size = new System.Drawing.Size(94, 26);
+            this.rbSpania.TabIndex = 4;
+            this.rbSpania.TabStop = true;
+            this.rbSpania.Text = "Spania";
+            this.rbSpania.UseVisualStyleBackColor = true;
+            // 
+            // rbAnglia
+            // 
+            this.rbAnglia.AutoSize = true;
+            this.rbAnglia.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbAnglia.ForeColor = System.Drawing.Color.Black;
+            this.rbAnglia.Location = new System.Drawing.Point(25, 81);
+            this.rbAnglia.Name = "rbAnglia";
+            this.rbAnglia.Size = new System.Drawing.Size(89, 26);
+            this.rbAnglia.TabIndex = 3;
+            this.rbAnglia.TabStop = true;
+            this.rbAnglia.Text = "Anglia";
+            this.rbAnglia.UseVisualStyleBackColor = true;
+            // 
+            // rbRomania
+            // 
+            this.rbRomania.AutoSize = true;
+            this.rbRomania.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbRomania.ForeColor = System.Drawing.Color.Black;
+            this.rbRomania.Location = new System.Drawing.Point(25, 21);
+            this.rbRomania.Name = "rbRomania";
+            this.rbRomania.Size = new System.Drawing.Size(111, 26);
+            this.rbRomania.TabIndex = 2;
+            this.rbRomania.TabStop = true;
+            this.rbRomania.Text = "Romania";
+            this.rbRomania.UseVisualStyleBackColor = true;
+            // 
+            // gbGrup
+            // 
+            this.gbGrup.BackColor = System.Drawing.Color.Gray;
+            this.gbGrup.Controls.Add(this.rbServici);
+            this.gbGrup.Controls.Add(this.rbFamilie);
+            this.gbGrup.Controls.Add(this.rbPrieteni);
+            this.gbGrup.Controls.Add(this.rbScoala);
+            this.gbGrup.Location = new System.Drawing.Point(85, 318);
+            this.gbGrup.Name = "gbGrup";
+            this.gbGrup.Size = new System.Drawing.Size(460, 143);
+            this.gbGrup.TabIndex = 5;
+            this.gbGrup.TabStop = false;
+            // 
+            // rbServici
+            // 
+            this.rbServici.AutoSize = true;
+            this.rbServici.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbServici.ForeColor = System.Drawing.Color.Black;
+            this.rbServici.Location = new System.Drawing.Point(268, 94);
+            this.rbServici.Name = "rbServici";
+            this.rbServici.Size = new System.Drawing.Size(94, 26);
+            this.rbServici.TabIndex = 6;
+            this.rbServici.TabStop = true;
+            this.rbServici.Text = "Servici";
+            this.rbServici.UseVisualStyleBackColor = true;
+            // 
+            // rbFamilie
+            // 
+            this.rbFamilie.AutoSize = true;
+            this.rbFamilie.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbFamilie.ForeColor = System.Drawing.Color.Black;
+            this.rbFamilie.Location = new System.Drawing.Point(268, 21);
+            this.rbFamilie.Name = "rbFamilie";
+            this.rbFamilie.Size = new System.Drawing.Size(96, 26);
+            this.rbFamilie.TabIndex = 5;
+            this.rbFamilie.TabStop = true;
+            this.rbFamilie.Text = "Familie";
+            this.rbFamilie.UseVisualStyleBackColor = true;
+            // 
+            // rbPrieteni
+            // 
+            this.rbPrieteni.AutoSize = true;
+            this.rbPrieteni.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbPrieteni.ForeColor = System.Drawing.Color.Black;
+            this.rbPrieteni.Location = new System.Drawing.Point(43, 92);
+            this.rbPrieteni.Name = "rbPrieteni";
+            this.rbPrieteni.Size = new System.Drawing.Size(102, 26);
+            this.rbPrieteni.TabIndex = 4;
+            this.rbPrieteni.TabStop = true;
+            this.rbPrieteni.Text = "Prieteni";
+            this.rbPrieteni.UseVisualStyleBackColor = true;
+            // 
+            // rbScoala
+            // 
+            this.rbScoala.AutoSize = true;
+            this.rbScoala.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbScoala.ForeColor = System.Drawing.Color.Black;
+            this.rbScoala.Location = new System.Drawing.Point(43, 21);
+            this.rbScoala.Name = "rbScoala";
+            this.rbScoala.Size = new System.Drawing.Size(93, 26);
+            this.rbScoala.TabIndex = 2;
+            this.rbScoala.TabStop = true;
+            this.rbScoala.Text = "Scoala";
+            this.rbScoala.UseVisualStyleBackColor = true;
+            // 
+            // labelProvenienta
+            // 
+            this.labelProvenienta.BackColor = System.Drawing.Color.Transparent;
+            this.labelProvenienta.Font = new System.Drawing.Font("Algerian", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProvenienta.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.labelProvenienta.Location = new System.Drawing.Point(264, 18);
+            this.labelProvenienta.Name = "labelProvenienta";
+            this.labelProvenienta.Size = new System.Drawing.Size(185, 33);
+            this.labelProvenienta.TabIndex = 6;
+            this.labelProvenienta.Text = "Provenienta";
+            this.labelProvenienta.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(216, 18);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // labelGrup
+            // 
+            this.labelGrup.BackColor = System.Drawing.Color.Transparent;
+            this.labelGrup.Font = new System.Drawing.Font("Algerian", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGrup.ForeColor = System.Drawing.Color.Black;
+            this.labelGrup.Location = new System.Drawing.Point(306, 271);
+            this.labelGrup.Name = "labelGrup";
+            this.labelGrup.Size = new System.Drawing.Size(82, 33);
+            this.labelGrup.TabIndex = 8;
+            this.labelGrup.Text = "Grup";
+            this.labelGrup.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(268, 259);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonFinalizare
+            // 
+            this.buttonFinalizare.BackColor = System.Drawing.Color.Black;
+            this.buttonFinalizare.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonFinalizare.ForeColor = System.Drawing.Color.Silver;
+            this.buttonFinalizare.Location = new System.Drawing.Point(226, 477);
+            this.buttonFinalizare.Name = "buttonFinalizare";
+            this.buttonFinalizare.Size = new System.Drawing.Size(190, 37);
+            this.buttonFinalizare.TabIndex = 10;
+            this.buttonFinalizare.Text = "Finalizare";
+            this.buttonFinalizare.UseVisualStyleBackColor = false;
+            this.buttonFinalizare.Click += new System.EventHandler(this.buttonFinalizare_Click);
+            // 
+            // Forma_Destinatie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(646, 526);
+            this.ControlBox = false;
+            this.Controls.Add(this.buttonFinalizare);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelGrup);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.labelProvenienta);
+            this.Controls.Add(this.gbGrup);
+            this.Controls.Add(this.gbProvenienta);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(700, 60);
+            this.Name = "Forma_Destinatie";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Load += new System.EventHandler(this.Forma_Destinatie_Load);
+            this.gbProvenienta.ResumeLayout(false);
+            this.gbProvenienta.PerformLayout();
+            this.gbGrup.ResumeLayout(false);
+            this.gbGrup.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox gbProvenienta;
+        private System.Windows.Forms.RadioButton rbOlanda;
+        private System.Windows.Forms.RadioButton rbItalia;
+        private System.Windows.Forms.RadioButton rbGermania;
+        private System.Windows.Forms.RadioButton rbSpania;
+        private System.Windows.Forms.RadioButton rbAnglia;
+        private System.Windows.Forms.RadioButton rbRomania;
+        private System.Windows.Forms.GroupBox gbGrup;
+        private System.Windows.Forms.RadioButton rbServici;
+        private System.Windows.Forms.RadioButton rbFamilie;
+        private System.Windows.Forms.RadioButton rbPrieteni;
+        private System.Windows.Forms.RadioButton rbScoala;
+        private System.Windows.Forms.Label labelProvenienta;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label labelGrup;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonFinalizare;
+    }
+}
